e7964d8bcd99fc5633ab44445ca0dfda256770c0
Jerrod Jaygbay <jerrodjaygbay123@gmail.com>
2026-04-13 18:17:28 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True 
+       
+        
+        newstr = ""
+        for ch in s:
+            if ch.isalnum():
+                newstr += ch.lower()
+
+        l = 0
+        r = len(newstr) - 1
+
+        while l < r:
+            if newstr[l] != newstr[r]:
+                return False
+            l += 1
+            r -= 1
+        return True 
+
+        
