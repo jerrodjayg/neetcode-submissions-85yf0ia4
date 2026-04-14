183c309ebf68be7e758b5ff04b522c2b395d084c
Jerrod Jaygbay <jerrodjaygbay123@gmail.com>
2026-04-13 23:05:51 -0400

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        close = {")" : "(", "}" : "{", "]" : "["}
+        stack = []
+
+
+        for p in s:
+            if p in close:
+                if stack and stack[-1] == close[p]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(p)
+
+        if not stack:
+            return True
+        else:
+            return False
+        
